3784e9ed094c9077b819b4a9103186fbe669beee
Julitosh07 <julito240907@gmail.com>
2025-07-23 18:34:08 -0500

    configurar lista tiendas

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from myapp import views
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('index', TemplateView.as_view(template_name='index.html'), name='index'),
     path('cacique', TemplateView.as_view(template_name='cacique.html'), name='cacique'),
     path('megamall', TemplateView.as_view(template_name='megamall.html'), name='megamall'),
     path('laquinta', TemplateView.as_view(template_name='laquinta.html'), name='laquinta'),
@@ -12,6 +12,6 @@ urlpatterns = [
     path('caracoli', TemplateView.as_view(template_name='caracoli.html'), name='caracoli'),
     path('laflorida', TemplateView.as_view(template_name='laflorida.html'), name='laflorida'),
     path('delacuesta', TemplateView.as_view(template_name='delacuesta.html'), name='delacuesta'),
-    path('centros_comerciales', views.lista_centros_comerciales, name='lista_centros_comerciales'),
+    path('', views.lista_centros_comerciales, name='lista_centros_comerciales'),
     path('tiendas', views.lista_tiendas, name='lista_tiendas'),
 ]
